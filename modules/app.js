47956f380a7652aa764fb4eb54764b3ab52a760a
Svetlana Zlt <svetlalanaz@gmail.com>
2026-03-20 00:59:30 +0300

    Changes

---
+++ modules/app.js
@@ -0,0 +1,6 @@
+import {readGameSaving as loadGame, writeGameSaving as saveGame};
+import * as gameAll from './game.js';
+
+
+const game = new Game();
+game.start();
---
+++ modules/game.js
@@ -0,0 +1,25 @@
+import {Character} from './domain.js';
+
+export default class Game {}
+
+export class GameSavingData {}
+
+export function readGameSaving {}
+
+export function writeGameSaving {}
+
+
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
